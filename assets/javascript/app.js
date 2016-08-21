2b3d3dab955d3325dbc24c2464e7dca28be24f2c
Kai <huikaizhong@yahoo.com>
2016-08-20 21:09:23 -0400

    working on formula

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,43 +10,68 @@
 // Create a variable to reference the database
 var database = firebase.database();
 
+//Create variables to hold user input
+var trainNameInput = $('#trainNameInput').val().trim();
+var destinationInput = $('#destinationInput').val().trim();
+var startTimeInput = $('#startTimeInput').val().trim();
+var frequencyInput = $('#frequencyInput').val().trim();
+
 // Capture Button Click
 $("#submitTrain").on("click", function() {
-	if (('#trainNameInput'&&'#destinationInput'&&'#startTimeInput'&&'#frequencyInput')!=null){
+	//if the input fields are not empty
+	if( trainNameInput != "" &&
+		destinationInput != "" &&
+		startTimeInput != "" &&
+		frequencyInput != "" ){
+	//push the data to firebase
 	database.ref().push({
-		trainName: $('#trainNameInput').val().trim(),
-		destination: $('#destinationInput').val().trim(),
-		startTime: $('#startTimeInput').val().trim(),
-		frequency: $('#frequencyInput').val().trim()
-	})};
-
+		trainName: trainNameInput,
+		destination: destinationInput,
+		startTime: startTimeInput,
+		frequency: frequencyInput
+	});}
+	//otherwise don't submit
 	else {
 		return false;
 	}
 
 	// Don't refresh the page!
-
+	return false;
 })
 
 //Firebase watcher + initial loader HINT: .on("value")
 database.ref().on("child_added", function(childSnapshot) {
 
-	// Log everything that's coming out of snapshot
-	// console.log(childSnapshot.val());
-	// console.log(childSnapshot.val().trainName);
-	// console.log(childSnapshot.val().destination);
-	// console.log(childSnapshot.val().startTime);
-	// console.log(childSnapshot.val().frequency);
-
+	//create rows to hold the capturing value inputs
 	var $trainBody = $('#trainRows');
-
 	var $trainRow = $('<tr>');
-
 	var $trainName = $('<td>').html(childSnapshot.val().trainName).appendTo($trainRow);
 	var $destination = $('<td>').html(childSnapshot.val().destination).appendTo($trainRow);
 	var $frequency = $('<td>').html(childSnapshot.val().frequency).appendTo($trainRow);
 	
-	
+	 //Set start time for each train
+    var startTime = moment(startTime, 'hh:mm');
+    console.log("start time: " + startTime);
+
+    //current time to find out difference
+    var currentTime = moment();
+    console.log("current time is: " + moment(currentTime).format("hh:mm"));
+
+    //total time between initial and current
+    var difference = moment().diff(moment(startTime), "minutes");
+    console.log("difference in initial time and current time: " + difference);
+
+    //use modular to figure out time 
+    //var remainder = difference % frequency;
+    //console.log("modular: " + remainder);
+
+    //minutes away time calculation
+    //var minutesAway = frequency - remainder;
+    //console.log("minutes away: " + minutesAway);
+
+    //calculate the next train arrival time
+    //var nextTrain = moment().add(minutesAway, "minutes");
+    //console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
 		
 	$trainRow.appendTo($trainBody);
 
